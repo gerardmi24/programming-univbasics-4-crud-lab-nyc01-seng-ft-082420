5faa84c33048f3e90f5615fa4d3cc560dc3e9b47
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 20:16:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,7 @@ def create_an_empty_array
   []
 end
 
-def create_an_array
-= [dogs, cats, birds, pigs]
+def create_an_array = [dogs, cats, birds, pigs]
 end
 
 def add_element_to_end_of_array(array, element)
