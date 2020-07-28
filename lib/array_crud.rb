252f9efe7d78ff77cebc9eaee2ed9c4e232e2e53
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 20:39:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,8 @@ array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[5]
 end
 
 def update_element_from_index(array, index_number, element)
