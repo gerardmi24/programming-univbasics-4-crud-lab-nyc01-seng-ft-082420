1d5ef11527819bab6c62a334c9ef1289856ced73
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 20:26:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ pets.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-
+  pets = ["dogs", "cats", "birds", "pigs"]
+  pigs = pets.pop
 end
 
 def remove_element_from_start_of_array(array)
