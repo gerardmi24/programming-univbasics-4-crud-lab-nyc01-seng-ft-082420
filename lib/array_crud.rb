d272fe8be3f4ccff68652295d19404d694192d8a
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 20:23:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
 pets = ["dogs", "cats", "birds", "pigs"]
-pets.unshift("owls")
+pets.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
